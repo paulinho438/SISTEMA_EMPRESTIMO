35472ba9094b8830cd477b0e320b945a950355da
Paulo Peixoto <paulopeixoto@lampp-it.com.br>
2024-03-30 17:00:41 -0300

    'correcao'

--- site/vite.config.js
+++ site/vite.config.js
@@ -1,6 +1,4 @@
 import { fileURLToPath, URL } from 'node:url';
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
@@ -8,14 +6,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig(() => {
     return {
-        plugins: [
-            vue(),
-            new CopyWebpackPlugin({
-                patterns: [
-                  { from: '.htaccess', to: '' }, // Copiar o .htaccess para a raiz do diretório de saída (dist)
-                ],
-            }),
-        ],
+        plugins: [vue()],
         resolve: {
             alias: {
                 '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -23,4 +14,3 @@ export default defineConfig(() => {
         }
     };
 });
-
