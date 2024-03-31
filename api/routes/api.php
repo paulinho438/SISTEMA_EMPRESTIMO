129791df922200a9e9a5bfa94bf6bb913cd64edc
Paulo Peixoto <paulopeixoto@lampp-it.com.br>
2024-03-30 21:37:58 -0300

    'teste'

--- api/routes/api.php
+++ api/routes/api.php
@@ -36,7 +36,7 @@ Route::post('/auth/login', [AuthController::class, 'login']);
 Route::post('/auth/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:api')->group(function(){
-    Route::post('/auth/validate', [AuthController::class, 'validateToken']);
+    Route::get('/auth/validate', [AuthController::class, 'validateToken']);
     Route::post('/auth/logout', [AuthController::class, 'logout']);
 
     Route::get('/permission_groups', [PermgroupController::class, 'index']);
