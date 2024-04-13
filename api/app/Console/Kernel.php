c9b01051d15633e2c82270d245c0f454832ebbed
Paulo Peixoto <paulopeixoto@lampp-it.com.br>
2024-04-13 13:12:22 -0300

    correcao

--- api/app/Console/Kernel.php
+++ api/app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('envio:EnvioManual')->everyMinute();
+        $schedule->command('baixa:Automatica')->everyMinute();
     }
 
     /**
