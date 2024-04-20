23e0c3fd5d2f49a1006d1a9d54c2a9a10ea63fc4
Paulo Peixoto <paulopeixoto@lampp-it.com.br>
2024-04-20 11:10:30 -0300

    'teste'

--- whatsapp-api/app.js
+++ whatsapp-api/app.js
@@ -15,9 +15,6 @@ let isClientLoggedIn = false;
 // Configurações do cliente do WhatsApp
 const client = new Client({
   puppeteer: {args: ["--no-sandbox"]},
-  authStrategy: new LocalAuth({
-  dataPath: "sessions",
-  }),
   webVersionCache: {
   type: 'remote',
   remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
