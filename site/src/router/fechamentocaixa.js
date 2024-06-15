db65e7805b952bd821e7bc03c6e7a5b3f37ca8da
Paulo Peixoto <paulopeixoto@lampp-it.com.br>
2024-06-15 14:03:19 -0300

    'correcao'

--- site/src/router/fechamentocaixa.js
+++ site/src/router/fechamentocaixa.js
@@ -7,16 +7,6 @@ const fechamentocaixaRoutes = [
 				path: '/fechamentocaixa',
 				name: 'fechamentocaixaList',
 				component: () => import('@/views/fechamentocaixa/FechamentoCaixaList.vue')
-			},
-			{
-				path: '/fechamentocaixa/:id/edit',
-				name: 'fechamentocaixaEdit',
-				component: () => import('@/views/fechamentocaixa/FeriadosForm.vue')
-			},
-			{
-				path: '/fechamentocaixa/add',
-				name: 'fechamentocaixaAdd',
-				component: () => import('@/views/fechamentocaixa/FeriadosForm.vue')
 			}
 		]
 	}
