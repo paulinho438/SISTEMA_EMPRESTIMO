ccfd4f162e90de233bb696c7ec709aefaba7d2ff
Paulo Peixoto <paulopeixoto@lampp-it.com.br>
2024-07-13 20:35:35 -0300

    a

--- api/app/Console/Commands/RecalcularParcelas.php
+++ api/app/Console/Commands/RecalcularParcelas.php
@@ -90,6 +90,39 @@ class RecalcularParcelas extends Command
 
                 }
 
+                if ($parcela->emprestimo->quitacao->chave_pix) {
+
+                    $parcela->emprestimo->quitacao->valor = $parcela->emprestimo->quitacao->valor + $valorJuros;
+                    $parcela->emprestimo->quitacao->saldo = $parcela->emprestimo->quitacao->saldo + $valorJuros;
+                    $parcela->emprestimo->quitacao->save();
+
+                    $gerarPixQuitacao = self::gerarPixQuitacao(
+                        [
+                            'banco' => [
+                                'client_id' => $parcela->emprestimo->banco->clienteid,
+                                'client_secret' => $parcela->emprestimo->banco->clientesecret,
+                                'certificado' => $parcela->emprestimo->banco->certificado,
+                                'chave' => $parcela->emprestimo->banco->chavepix,
+                            ],
+                            'parcela' => [
+                                'parcela' => $parcela->parcela,
+                                'valor' => $parcela->emprestimo->quitacao->saldo,
+                                'venc_real' => date('Y-m-d'),
+                            ],
+                            'cliente' => [
+                                'nome_completo' => $parcela->emprestimo->client->nome_completo,
+                                'cpf' => $parcela->emprestimo->client->cpf
+                            ]
+                        ]
+                    );
+
+                    $parcela->emprestimo->quitacao->identificador = $gerarPixQuitacao['identificador'];
+                    $parcela->emprestimo->quitacao->chave_pix = $gerarPixQuitacao['chave_pix'];
+
+                    $parcela->emprestimo->quitacao->save();
+
+                }
+
                 if ($parcela->contasreceber) {
                     $parcela->contasreceber->venc = $parcela->venc_real;
                     $parcela->contasreceber->valor = $parcela->saldo;
@@ -97,106 +130,6 @@ class RecalcularParcelas extends Command
                 }
                 $parcela->save();
 
-                // recalculando o valor total a quitar emprestimo
-
-                $parcela->emprestimo->quitacao->valor = $parcela->emprestimo->quitacao->valor + $valorJuros;
-                $parcela->emprestimo->quitacao->saldo = $parcela->emprestimo->quitacao->saldo + $valorJuros;
-                $parcela->emprestimo->quitacao->save();
-
-                $caminhoAbsoluto = storage_path('app/public/documentos/' . $parcela->emprestimo->banco->certificado);
-
-                $options = [
-                    'clientId' => $parcela->emprestimo->banco->clienteid,
-                    'clientSecret' => $parcela->emprestimo->banco->clientesecret,
-                    'certificate' => $caminhoAbsoluto,
-                    'sandbox' => false,
-                    "debug" => false,
-                    'timeout' => 60,
-                ];
-
-                $params = [
-                    "txid" => Str::random(32)
-                ];
-
-                $body = [
-                    "calendario" => [
-                        "dataDeVencimento" => date('Y-m-d'),
-                        "validadeAposVencimento" => 0
-                    ],
-                    "devedor" => [
-                        "nome_completo" => $parcela->emprestimo->client->nome_completo,
-                        "cpf" => str_replace(['-', '.'], '', $parcela->emprestimo->client->cpf),
-                    ],
-                    "valor" => [
-                        "original" => number_format(str_replace(',', '', $parcela->emprestimo->quitacao->saldo), 2, '.', ''),
-
-                    ],
-                    "chave" => $parcela->emprestimo->banco->chavepix, // Pix key registered in the authenticated Efí account
-                    "solicitacaoPagador" => "Quitação do Emprestimo ",
-                    "infoAdicionais" => [
-                        [
-                            "nome" => "Emprestimo",
-                            "valor" => "R$ " . $parcela->emprestimo->quitacao->saldo,
-                        ]
-                    ]
-                ];
-
-                try {
-                    $api = new EfiPay($options);
-                    $pix = $api->pixCreateDueCharge($params, $body);
-
-                    if ($pix["txid"]) {
-                        $params = [
-                            "id" => $pix["loc"]["id"]
-                        ];
-
-                        $parcela->emprestimo->quitacao->identificador = $pix["loc"]["id"];
-
-
-                        try {
-                            $qrcode = $api->pixGenerateQRCode($params);
-
-                            $parcela->emprestimo->quitacao->chave_pix = $qrcode['linkVisualizacao'];
-
-                            $parcela->emprestimo->quitacao->save();
-                        } catch (EfiException $e) {
-
-                            CustomLog::create([
-                                'user_id' => 0,
-                                'content' => 'Error ao gerar a quitacao ' . $e->code . ' ' . $e->error . ' ' . $e->errorDescription . ' REQUEST '. json_encode($body, JSON_PRETTY_PRINT),
-                                'operation' => 'error'
-                            ]);
-
-                            print_r($e->code . "<br>");
-                            print_r($e->error . "<br>");
-                            print_r($e->errorDescription) . "<br>";
-                        } catch (Exception $e) {
-                            CustomLog::create([
-                                'user_id' => 0,
-                                'content' => $e->getMessage(),
-                                'operation' => 'error'
-                            ]);
-                        }
-                    } else {
-                        CustomLog::create([
-                            'user_id' => 0,
-                            'content' => "<pre>" . json_encode($pix, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) . "</pre>",
-                            'operation' => 'error'
-                        ]);
-                    }
-                } catch (EfiException $e) {
-                    CustomLog::create([
-                        'user_id' => 0,
-                        'content' => 'Error ao gerar a quitacao ' . $e->code . ' ' . $e->error . ' ' . $e->errorDescription . ' REQUEST '. json_encode($body, JSON_PRETTY_PRINT),
-                        'operation' => 'error'
-                    ]);
-                } catch (Exception $e) {
-                    CustomLog::create([
-                        'user_id' => 0,
-                        'content' => $e->getMessage(),
-                        'operation' => 'error'
-                    ]);
-                }
 
             }
         }
@@ -205,6 +138,92 @@ class RecalcularParcelas extends Command
     }
 
     public function gerarPix($dados)
+    {
+
+        $return = [];
+
+        $caminhoAbsoluto = storage_path('app/public/documentos/' . $dados['banco']['certificado']);
+        $conteudoDoCertificado = file_get_contents($caminhoAbsoluto);
+        $options = [
+            'clientId' => $dados['banco']['client_id'],
+            'clientSecret' => $dados['banco']['client_secret'],
+            'certificate' => $caminhoAbsoluto,
+            'sandbox' => false,
+            "debug" => false,
+            'timeout' => 60,
+        ];
+
+        $params = [
+            "txid" => Str::random(32)
+        ];
+
+        $body = [
+            "calendario" => [
+                "dataDeVencimento" => $dados['parcela']['venc_real'],
+                "validadeAposVencimento" => 0
+            ],
+            "devedor" => [
+                "nome" => $dados['cliente']['nome_completo'],
+                "cpf" => str_replace(['-', '.'], '', $dados['cliente']['cpf']),
+            ],
+            "valor" => [
+                "original" => number_format(str_replace(',', '', $dados['parcela']['valor']), 2, '.', ''),
+
+            ],
+            "chave" => $dados['banco']['chave'], // Pix key registered in the authenticated Efí account
+            "solicitacaoPagador" => "Parcela " . $dados['parcela']['parcela'],
+            "infoAdicionais" => [
+                [
+                    "nome" => "Emprestimo",
+                    "valor" => "R$ " . $dados['parcela']['valor'],
+                ],
+                [
+                    "nome" => "Parcela",
+                    "valor" => $dados['parcela']['parcela']
+                ]
+            ]
+        ];
+
+        try {
+            $api = new EfiPay($options);
+            $pix = $api->pixCreateDueCharge($params, $body);
+
+
+            if ($pix["txid"]) {
+                $params = [
+                    "id" => $pix["loc"]["id"]
+                ];
+
+                $return['identificador'] = $pix["loc"]["id"];
+
+
+                try {
+                    $qrcode = $api->pixGenerateQRCode($params);
+
+                    $return['chave_pix'] = $qrcode['linkVisualizacao'];
+
+                    return $return;
+
+                } catch (EfiException $e) {
+                    print_r($e->code . "<br>");
+                    print_r($e->error . "<br>");
+                    print_r($e->errorDescription) . "<br>";
+                } catch (Exception $e) {
+                    print_r($e->getMessage());
+                }
+            } else {
+                echo "<pre>" . json_encode($pix, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) . "</pre>";
+            }
+        } catch (EfiException $e) {
+            print_r($e->code . "<br>");
+            print_r($e->error . "<br>");
+            print_r($e->errorDescription) . "<br>";
+        } catch (Exception $e) {
+            print_r($e->getMessage());
+        }
+    }
+
+    public function gerarPixQuitacao($dados)
     {
 
         $return = [];
