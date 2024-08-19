1217f3f0d3609c3f089750001e3c6365aaa052d3
Paulo Peixoto <paulopeixoto@lampp-it.com.br>
2024-08-19 01:12:48 -0300

    'a'

--- api/database/migrations/2014_10_12_000000_create_users_table.php
+++ api/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
         Schema::create('users', function(Blueprint $table) {
             $table->id();
             $table->string('nome_completo', 150);
-            $table->string('cpf', 20)->unique();
-            $table->string('rg', 20)->unique();
+            $table->string('cpf', 20);
+            $table->string('rg', 20);
             $table->date('data_nascimento');
             $table->enum('sexo', ['M', 'F'])->comment('Valores válidos ["M","F"]');
             $table->string('telefone_celular', 20);
