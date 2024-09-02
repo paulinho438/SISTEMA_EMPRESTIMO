2f86a8a0ef3096e42aa298beb8b6714e586f6d2a
Paulo Peixoto <paulopeixoto@lampp-it.com.br>
2024-09-01 21:40:09 -0300

    a

--- site/src/views/emprestimos/EmprestimosAdd.vue
+++ site/src/views/emprestimos/EmprestimosAdd.vue
@@ -86,9 +86,9 @@ export default {
 		gerarParcelas() {
 			let parcela = {};
 			// Defina a data inicial
-			const dataLanc = this.address.dt_lancamento;
+			const dataLanc = new Date(this.address.dt_lancamento);
 
-			const dataInicial = this.address.dt_lancamento;
+			const dataInicial = new Date(this.address.dt_lancamento);
 
 			// Array para armazenar as parcelas
 			const parcelas = [];
