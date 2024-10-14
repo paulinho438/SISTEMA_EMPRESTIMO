c0a60363a9ac14280999173af0a65373ed80c08e
Paulo Peixoto <paulo.peixoto@gs3tecnologia.com.br>
2024-10-14 20:12:11 -0300

    criacao pagamento minimo

---
+++ api/app/Models/PagamentoMinimo.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PagamentoMinimo extends Model
+{
+    public $table = 'pagamento_minimo';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'emprestimo_id',
+        'valor',
+        'dt_baixa',
+        'identificador',
+        'chave_pix',
+    ];
+
+    public function emprestimo()
+    {
+        return $this->belongsTo(Emprestimo::class, 'emprestimo_id', 'id');
+    }
+
+}
