6f7f36e59934f740bc216eab82f859e9dc7076e0
Paulo Peixoto <paulo.peixoto@gs3tecnologia.com.br>
2024-10-26 09:11:58 -0300

    a

---
+++ api/database/migrations/2024_10_26_091106_add_email_to_companies_table.php
@@ -0,0 +1,31 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddEmailToCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('companies', function (Blueprint $table) {
+            $table->string('email')->nullable()->after('name'); // Adiciona a coluna 'email' após a coluna 'name'
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('companies', function (Blueprint $table) {
+            $table->dropColumn('email'); // Remove a coluna 'email' se a migration for revertida
+        });
+    }
+}
