20c540826da71ec00056ab6cc5f412b32972e2f5
Paulo Peixoto <paulo.peixoto@gs3tecnologia.com.br>
2024-11-03 09:16:44 -0300

    a

--- api/database/seeders/CompanySeeder.php
+++ api/database/seeders/CompanySeeder.php
@@ -15,17 +15,43 @@ class CompanySeeder extends Seeder
      */
     public function run()
     {
+        DB::table('planos')->insert([
+            'nome' => 'Acima de 100 contratos / mês',
+            'preco' => 99.99,
+            'descricao' => 'Plano básico com funcionalidades limitadas',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('planos')->insert([
+            'nome' => 'Plano intermediario',
+            'preco' => 50,
+            'descricao' => 'Plano intermediario',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('planos')->insert([
+            'nome' => 'Plano Básico',
+            'preco' => 30,
+            'descricao' => 'Plano básico com funcionalidades limitadas',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
         DB::table("companies")->insert(
             [
                 "company" => "BSB EMPRESTIMOS",
                 "juros" => 0.5,
                 "whatsapp" => "https://node1.rjemprestimos.com.br",
+                "plano_id" => 1
             ]
         );
 
         DB::table("companies")->insert(
             [
                 "company" => "RJ EMPRESTIMOS",
+                "plano_id" => 1
             ]
         );
     }
