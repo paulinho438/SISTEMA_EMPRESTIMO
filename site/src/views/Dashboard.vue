59ac1c5dde96374f5dfb91060bd3ccb7b97979c2
Paulo Peixoto <paulo.peixoto@gs3tecnologia.com.br>
2024-11-10 12:15:49 -0300

    a

--- site/src/views/Dashboard.vue
+++ site/src/views/Dashboard.vue
@@ -250,51 +250,7 @@ watch(
                 <span class="text-500">since last visit</span> -->
             </div>
         </div>
-        <div class="col-12 lg:col-6 xl:col-3">
-            <div class="card mb-0">
-                <div class="flex justify-content-between mb-3">
-                    <div>
-                        <span class="block text-500 font-medium mb-3">Empréstimos Pagos</span>
-                        <div class="text-900 font-medium text-xl">{{ infoConta?.total_emprestimos_pagos ?? 'Carregando...' }}</div>
-                    </div>
-                    <div class="flex align-items-center justify-content-center bg-orange-100 border-round" style="width: 2.5rem; height: 2.5rem">
-                        <i class="pi pi-map-marker text-orange-500 text-xl"></i>
-                    </div>
-                </div>
-                <!-- <span class="text-green-500 font-medium">%52+ </span>
-                <span class="text-500">since last week</span> -->
-            </div>
-        </div>
-        <div class="col-12 lg:col-6 xl:col-3">
-            <div class="card mb-0">
-                <div class="flex justify-content-between mb-3">
-                    <div>
-                        <span class="block text-500 font-medium mb-3">Empréstimos em dias</span>
-                        <div class="text-900 font-medium text-xl">{{ infoConta?.total_emprestimos_em_dias ?? 'Carregando...' }}</div>
-                    </div>
-                    <div class="flex align-items-center justify-content-center bg-cyan-100 border-round" style="width: 2.5rem; height: 2.5rem">
-                        <i class="pi pi-inbox text-cyan-500 text-xl"></i>
-                    </div>
-                </div>
-                <!-- <span class="text-green-500 font-medium">520 </span>
-                <span class="text-500">newly registered</span> -->
-            </div>
-        </div>
-        <div class="col-12 lg:col-6 xl:col-3">
-            <div class="card mb-0">
-                <div class="flex justify-content-between mb-3">
-                    <div>
-                        <span class="block text-500 font-medium mb-3">Empréstimos atrasados</span>
-                        <div class="text-900 font-medium text-xl">{{ infoConta?.total_emprestimos_muito_atrasados ?? 'Carregando...' }}</div>
-                    </div>
-                    <div class="flex align-items-center justify-content-center bg-purple-100 border-round" style="width: 2.5rem; height: 2.5rem">
-                        <i class="pi pi-comment text-purple-500 text-xl"></i>
-                    </div>
-                </div>
-                <!-- <span class="text-green-500 font-medium">85 </span>
-                <span class="text-500">responded</span> -->
-            </div>
-        </div>
+      
 
         <!-- <div class="col-12 xl:col-6">
             <div class="card">
