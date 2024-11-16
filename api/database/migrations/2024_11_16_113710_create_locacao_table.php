ad696221daf77b7200d463b14cf220fbf2e4b03a
Paulo Peixoto <paulo.peixoto@gs3tecnologia.com.br>
2024-11-16 11:40:15 -0300

    a

---
+++ api/database/migrations/2024_11_16_113710_create_locacao_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLocacaoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locacao', function (Blueprint $table) {
+            $table->string('id')->primary(); // Campo id do tipo hash
+            $table->timestamps(); // Campos created_at e updated_at
+            $table->string('type');
+            $table->date('data_vencimento');
+            $table->date('data_pagamento')->nullable();
+            $table->decimal('valor', 10, 2);
+            $table->unsignedBigInteger('company_id');
+            $table->foreign('company_id')->references('id')->on('company');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locacao');
+    }
+}
