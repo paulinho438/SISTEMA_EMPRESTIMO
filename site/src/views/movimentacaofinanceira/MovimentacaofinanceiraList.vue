a1870d511c9e303ea88a592d8268ed19671d79af
Paulo Peixoto <paulo.peixoto@gs3tecnologia.com.br>
2024-11-23 08:52:04 -0300

    a

--- site/src/views/movimentacaofinanceira/MovimentacaofinanceiraList.vue
+++ site/src/views/movimentacaofinanceira/MovimentacaofinanceiraList.vue
@@ -94,6 +94,7 @@ export default {
 
 			const dt_inicio = new Date(this.form.dt_inicio);
 			const dt_final = new Date(this.form.dt_final);
+			dt_inicio.setHours(0, 0, 0, 0); // Ensure the end date covers the entire day
 			dt_final.setHours(23, 59, 59, 999); // Ensure the end date covers the entire day
 
 			this.Movimentacaofinanceira = this.MovimentacaofinanceiraReal.filter(mov => {
