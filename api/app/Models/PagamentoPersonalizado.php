373186973821e1c9130c7f83bdcdd892aea13224
Paulo Peixoto <paulo.peixoto@gs3tecnologia.com.br>
2024-11-24 10:19:15 -0300

    a

---
+++ api/app/Models/PagamentoPersonalizado.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PagamentoPersonalizado extends Model
+{
+    public $table = 'pagamento_personalizado';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'emprestimo_id',
+        'valor',
+        'saldo',
+        'dt_baixa',
+        'identificador',
+        'chave_pix',
+    ];
+
+    public function emprestimo()
+    {
+        return $this->belongsTo(Emprestimo::class, 'emprestimo_id', 'id');
+    }
+
+}
