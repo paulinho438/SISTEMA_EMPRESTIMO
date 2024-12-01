9e6e7de5fc09fef55758b02de6133ee522ad85fc
Paulo Peixoto <paulo.peixoto@gs3tecnologia.com.br>
2024-12-01 09:24:16 -0300

    a

--- api/app/Console/Commands/MensagemAutomaticaRenovacao.php
+++ api/app/Console/Commands/MensagemAutomaticaRenovacao.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Http;
 
 use App\Models\Juros;
 use App\Models\Parcela;
-use App\Models\Feriado;
+use App\Models\Client;
 
 use Efi\Exception\EfiException;
 use Efi\EfiPay;
@@ -43,144 +43,75 @@ class MensagemAutomaticaRenovacao extends Command
     public function handle()
     {
 
-        $this->info('Realizando a Cobrança Automatica das Parcelas em Atrasos');
+        $this->info('Realizando a Mensagem Renovacao Automatica');
 
-        // Obtendo a data de hoje no formato YYYY-MM-DD
-        $today = Carbon::today()->toDateString();
+        // Buscar clientes e seus empréstimos
+        $clients = Client::whereHas('emprestimos', function ($query) {
+            $query->whereHas('parcelas', function ($query) {
+                $query->whereNotNull('dt_baixa');
+            });
+        })
+            ->with(['emprestimos' => function ($query) {
+                $query->whereHas('parcelas', function ($query) {
+                    $query->whereNotNull('dt_baixa');
+                });
+            }])
+            ->get();
 
-        // Verificando se hoje é um feriado
-        $isHoliday = Feriado::where('data_feriado', $today)->exists();
-
-        $parcelas = collect(); // Coleção vazia se hoje for um feriado
-
-        if (!$isHoliday) {
-            $parcelas = Parcela::where('dt_baixa', null)
-                ->whereNull('valor_recebido_pix')
-                ->whereNull('valor_recebido')
-                ->whereDate('venc_real', $today)
-                ->get()
-                ->unique('emprestimo_id');
-        }
-
-
-        $r = [];
-        foreach ($parcelas as $parcela) {
-            if (isset($parcela->emprestimo->company->whatsapp) && $parcela->emprestimo->contaspagar && $parcela->emprestimo->contaspagar->status == "Pagamento Efetuado") {
-
-                try {
-
-                    $response = Http::get($parcela->emprestimo->company->whatsapp . '/logar');
-
-                    if ($response->successful()) {
-                        $r = $response->json();
-                        if ($r['loggedIn']) {
-
-                            $telefone = preg_replace('/\D/', '', $parcela->emprestimo->client->telefone_celular_1);
-                            $baseUrl = $parcela->emprestimo->company->whatsapp . '/enviar-mensagem';
-
-                            $saudacao = self::obterSaudacao();
-
-                            $parcelaPendente = self::encontrarPrimeiraParcelaPendente($parcela->emprestimo->parcelas);
-
-                            $saudacaoTexto = "{$saudacao}, " . $parcela->emprestimo->client->nome_completo . "!";
-                            $fraseInicial = "
-
-Relatório de Parcelas Pendentes:
-
-Segue abaixo link para pagamento parcela diária e acesso todo o histórico de parcelas:
-
-https://sistema.rjemprestimos.com.br/#/parcela/{$parcela->id}
-
-
-";
-
-if($parcelaPendente !=  null && $parcelaPendente->chave_pix != ''){
-    $fraseInicial .= "Copie e cole abaixo a chave pix e faça o pagamento de R$ ".$parcelaPendente->saldo." referente a parcela do dia:
-
-{$parcelaPendente->chave_pix}
-";
-}else{
-    $fraseInicial .= "Copie e cole abaixo a chave pix e faça o pagamento de R$ ".$parcelaPendente->saldo." referente a parcela do dia:
-
-Chave pix: {$parcela->emprestimo->banco->chavepix}
-";
-}
+        // Filtrar os resultados em PHP
+        $filteredClients = $clients->filter(function ($client) {
+            return $client->emprestimos->company->envio_automatico_renovacao == 1 && $client->emprestimos->mensagem_renovacao == 0;
+        });
 
 
 
-
-                            // Montagem das parcelas pendentes
-//                             $parcelasString = $parcela->emprestimo->parcelas
-//                                 ->filter(function ($item) {
-//                                     return $item->atrasadas > 0 && is_null($item->dt_baixa);
-//                                 })
-//                                 ->map(function ($item) {
-//                                     return "
-// Data: " . Carbon::parse($item->venc)->format('d/m/Y') . "
-// Parcela: {$item->parcela}
-// Atrasos: {$item->atrasadas}
-// Valor: R$ " . number_format($item->valor, 2, ',', '.') . "
-// Multa: R$ " . number_format(($item->saldo - $item->valor) ?? 0, 2, ',', '.') . "
-// Juros: R$ " . number_format($item->multa ?? 0, 2, ',', '.') . "
-// Pago: R$ " . number_format($item->pago ?? 0, 2, ',', '.') . "
-// PIX: " . ($item->chave_pix ?? 'Não Contém') . "
-// Status: Pendente
-// RESTANTE: R$ " . number_format($item->saldo, 2, ',', '.');
-//                                 })
-//                                 ->implode("\n\n");
-
-
-
-                            // Obtenha a saudação baseada na hora atual
-
-                            // $frase = $saudacaoTexto . $fraseInicial . $parcelasString;
-                            $frase = $saudacaoTexto . $fraseInicial;
-
-                            $data = [
-                                "numero" => "55" . $telefone,
-                                "mensagem" => $frase
-                            ];
-
-                            $response = Http::asJson()->post($baseUrl, $data);
-                            sleep(8);
-                        }
-                    }
-                } catch (\Throwable $th) {
-                    dd($th);
-
-                }
-
+        foreach ($filteredClients as $client) {
+            if ($client->emprestimos->count_late_parcels <= 2) {
+                self::enviarMensagem($client, 'Olá ' . $client['nome_completo'] . ', estamos entrando em contato para informar sobre seu empréstimo. Temos uma ótima notícia: você possui um valor pré-aprovado de R$ ' . ($client['emprestimos']['valor'] + 100) . ' Gostaria de contratar?');
             }
+
+            if ($client->emprestimos->count_late_parcels >= 3 && $client->emprestimos->count_late_parcels <= 5) {
+                self::enviarMensagem($client, 'Olá ' . $client['nome_completo'] . ', estamos entrando em contato para informar sobre seu empréstimo. Temos uma ótima notícia: você possui um valor pré-aprovado de R$ ' . ($client['emprestimos']['valor']) . ' Gostaria de contratar?');
+            }
+
+            if ($client->emprestimos->count_late_parcels >= 6) {
+                self::enviarMensagem($client, 'Olá ' . $client['nome_completo'] . ', estamos entrando em contato para informar sobre seu empréstimo. Temos uma ótima notícia: você possui um valor pré-aprovado de R$ ' . ($client['emprestimos']['valor'] - 100) . ' Gostaria de contratar?');
+            }
+
+            $client->emprestimos->mensagem_renovacao = 1;
+            $client->emprestimos->save();
+
         }
 
         exit;
     }
 
-    function obterSaudacao()
+    public function enviarMensagem($cliente, $frase)
     {
-        $hora = date('H');
-        $saudacoesManha = ['Bom dia', 'Olá, bom dia', 'Tenha um excelente dia'];
-        $saudacoesTarde = ['Boa tarde', 'Olá, boa tarde', 'Espero que sua tarde esteja ótima'];
-        $saudacoesNoite = ['Boa noite', 'Olá, boa noite', 'Espero que sua noite esteja ótima'];
+        try {
 
-        if ($hora < 12) {
-            return $saudacoesManha[array_rand($saudacoesManha)];
-        } elseif ($hora < 18) {
-            return $saudacoesTarde[array_rand($saudacoesTarde)];
-        } else {
-            return $saudacoesNoite[array_rand($saudacoesNoite)];
-        }
-    }
+            $response = Http::get($cliente->emprestimos->company->whatsapp . '/logar');
 
-    function encontrarPrimeiraParcelaPendente($parcelas) {
+            if ($response->successful()) {
+                $r = $response->json();
+                if ($r['loggedIn']) {
 
-        foreach($parcelas as $parcela){
-            if($parcela->dt_baixa == null){
-                return $parcela;
+                    $telefone = preg_replace('/\D/', '', $cliente->telefone_celular_1);
+                    $baseUrl = $cliente->emprestimos->company->whatsapp . '/enviar-mensagem';
+
+                    $data = [
+                        "numero" => "55" . $telefone,
+                        "mensagem" => $frase
+                    ];
+
+                    $response = Http::asJson()->post($baseUrl, $data);
+                    sleep(8);
+                }
             }
+        } catch (\Throwable $th) {
+            dd($th);
         }
 
-        return null;
+        return true;
     }
-
 }
