ea80b4c42bfe5403d2164360322b781423f8a6d8
Paulo Peixoto <paulo.peixoto@gs3tecnologia.com.br>
2024-12-15 08:17:13 -0300

    a

--- api/app/Http/Controllers/EmprestimoController.php
+++ api/app/Http/Controllers/EmprestimoController.php
@@ -1567,15 +1567,7 @@ class EmprestimoController extends Controller
                 ], Response::HTTP_FORBIDDEN);
             }
 
-            $movimentacaoFinanceira = [];
-            $movimentacaoFinanceira['banco_id'] = $permGroup->banco->id;
-            $movimentacaoFinanceira['company_id'] = $permGroup->company_id;
-            $movimentacaoFinanceira['descricao'] = 'Exclusão Empréstimo Nº ' . $permGroup->id;
-            $movimentacaoFinanceira['tipomov'] = 'E';
-            $movimentacaoFinanceira['dt_movimentacao'] = date('Y-m-d');
-            $movimentacaoFinanceira['valor'] = $permGroup->valor;
-
-            Movimentacaofinanceira::create($movimentacaoFinanceira);
+            $permGroup->contaspagar->delete();
 
             $permGroup->delete();
 
