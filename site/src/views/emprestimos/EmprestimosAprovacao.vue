b8245cf90f5f291133498d3fac9dcfca4d26786f
Paulo Peixoto <paulo.peixoto@gs3tecnologia.com.br>
2024-12-21 22:59:33 -0300

    a

--- site/src/views/emprestimos/EmprestimosAprovacao.vue
+++ site/src/views/emprestimos/EmprestimosAprovacao.vue
@@ -170,7 +170,7 @@ export default {
                             }
                         })
                         .finally(() => {
-                            this.changeLoading();
+                            this.loading = false;
                         });
                 } else {
                     this.emprestimoService
