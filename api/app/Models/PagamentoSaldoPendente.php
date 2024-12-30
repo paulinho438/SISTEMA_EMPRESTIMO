a98aefa1a85ca9a18dd211df3aeedcaa323688f6
Paulo Peixoto <paulo.peixoto@gs3tecnologia.com.br>
2024-12-30 10:15:00 -0300

    a

---
+++ api/app/Models/PagamentoSaldoPendente.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PagamentoSaldoPendente extends Model
+{
+    public $table = 'pagamento_saldo_pendente';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'emprestimo_id',
+        'valor',
+        'identificador',
+        'chave_pix',
+    ];
+
+    public function emprestimo()
+    {
+        return $this->belongsTo(Emprestimo::class, 'emprestimo_id', 'id');
+
+    }
+
+}
