09f49cd66b955995f9058ffa24e176313380685f
Paulo Peixoto <paulo.peixoto@gs3tecnologia.com.br>
2025-02-02 08:01:28 -0300

    a

--- api/app/Http/Controllers/EmprestimoController.php
+++ api/app/Http/Controllers/EmprestimoController.php
@@ -2094,11 +2094,11 @@ class EmprestimoController extends Controller
                     $movimentacaoFinanceira = [];
                     $movimentacaoFinanceira['banco_id'] = $deposito->banco_id;
                     $movimentacaoFinanceira['company_id'] = $deposito->company_id;
-                    // $movimentacaoFinanceira['descricao'] = sprintf(
-                    //     'Deposito %d',
-                    //     json_encode($data)
+                    $movimentacaoFinanceira['descricao'] = sprintf(
+                        'Deposito Pagador: %d',
+                        $data['pagador']['nome'] ?? 'Não informado'
 
-                    // );
+                    );
                     $movimentacaoFinanceira['descricao'] = json_encode($request->json()->all());
                     $movimentacaoFinanceira['tipomov'] = 'E';
                     $movimentacaoFinanceira['dt_movimentacao'] = date('Y-m-d');
