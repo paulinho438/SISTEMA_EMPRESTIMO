152f8bbe5afdd892e21b8924196016ff187f526a
Paulo Peixoto <paulo.peixoto@gs3tecnologia.com.br>
2025-02-02 11:19:51 -0300

    a

--- site/src/views/Dashboard.vue
+++ site/src/views/Dashboard.vue
@@ -188,8 +188,8 @@ watch(
                 </div>
                 <div class="flex justify-content-between mb-3" style="flex-direction: column; gap: 10px;">
                     <div>
-                        <span class="text-green-500 font-medium">Total de Clientes </span>
-                        <span class="text-500">{{ infoConta?.total_clientes }}</span>
+                        <span class="text-green-500 font-medium">Empréstimos Ativos</span>
+                        <span class="text-500">{{ infoConta?.total_emprestimos_em_dias + infoConta?.total_emprestimos_muito_atrasados }}</span>
                     </div>
                     <div>
                         <span class="text-green-500 font-medium">Valores Já Investidos </span>
@@ -266,8 +266,8 @@ watch(
             <div class="card mb-0">
                 <div class="flex justify-content-between mb-3">
                     <div>
-                        <span class="block text-500 font-medium mb-3">Empréstimos Ativos</span>
-                        <div class="text-900 font-medium text-xl">{{ infoConta?.total_emprestimos_em_dias + infoConta?.total_emprestimos_muito_atrasados ?? 'Carregando...' }}</div>
+                        <span class="block text-500 font-medium mb-3">Total de Clientes</span>
+                        <div class="text-900 font-medium text-xl">{{ infoConta?.total_clientes ?? 'Carregando...' }}</div>
                     </div>
                     <div class="flex align-items-center justify-content-center bg-blue-100 border-round" style="width: 2.5rem; height: 2.5rem">
                         <i class="pi pi-shopping-cart text-blue-500 text-xl"></i>
