e2dea0c2e5f4250a5f1de579469117007f4b701e
Paulo Peixoto <paulo.peixoto@gs3tecnologia.com.br>
2025-02-02 08:11:20 -0300

    a

--- api/app/Http/Controllers/EmprestimoController.php
+++ api/app/Http/Controllers/EmprestimoController.php
@@ -1602,22 +1602,22 @@ class EmprestimoController extends Controller
     {
         $data = $request->json()->all();
 
-        // // Nome do arquivo
-        // $file = 'webhookcobranca.txt';
+        // Nome do arquivo
+        $file = 'webhookcobranca.txt';
 
-        // // Verifica se o arquivo existe, se não, cria-o
-        // if (!Storage::exists($file)) {
-        //     Storage::put($file, '');
-        // }
+        // Verifica se o arquivo existe, se não, cria-o
+        if (!Storage::exists($file)) {
+            Storage::put($file, '');
+        }
 
-        // // Lê o conteúdo atual do arquivo
-        // $current = Storage::get($file);
+        // Lê o conteúdo atual do arquivo
+        $current = Storage::get($file);
 
-        // // Adiciona os novos dados ao conteúdo atual
-        // $current .= json_encode($data) . PHP_EOL;
+        // Adiciona os novos dados ao conteúdo atual
+        $current .= json_encode($data) . PHP_EOL;
 
-        // // Salva o conteúdo atualizado no arquivo
-        // Storage::put($file, $current);
+        // Salva o conteúdo atualizado no arquivo
+        Storage::put($file, $current);
 
         //REFERENTE A PARCELAS
         if (isset($data['pix']) && is_array($data['pix'])) {
