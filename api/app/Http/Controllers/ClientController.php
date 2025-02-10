fcf8b94114579f395e8aa99880eba0892fd9665c
Paulo Peixoto <paulo.peixoto@gs3tecnologia.com.br>
2025-02-09 22:03:16 -0300

    a

--- api/app/Http/Controllers/ClientController.php
+++ api/app/Http/Controllers/ClientController.php
@@ -74,7 +74,7 @@ class ClientController extends Controller
                 })
                 ->where(function ($query) use ($request, $companyIds) {
                     if (auth()->user()->getGroupNameByEmpresaId($request->header('company-id')) == 'Consultor') {
-                        $query->where('emprestimos.company_id', $request->header('company-id'));
+                        $query->where('company_id', $request->header('company-id'));
                     }else{
                         $query->whereIn('emprestimos.company_id', $companyIds); // Especifica a tabela emprestimos
                     }
