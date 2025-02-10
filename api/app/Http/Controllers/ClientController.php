50167a7c8334889b35f2128751d9c2a848435ed8
Paulo Peixoto <paulo.peixoto@gs3tecnologia.com.br>
2025-02-09 21:27:45 -0300

    a

--- api/app/Http/Controllers/ClientController.php
+++ api/app/Http/Controllers/ClientController.php
@@ -73,17 +73,19 @@ class ClientController extends Controller
                 }
             })
 
+            ->where(function ($query) use ($request) {
+                if (auth()->user()->getGroupNameByEmpresaId(empresaId: $request->header('company-id')) != 'Consultor') {
+                    $query->where('company_id',  $request->header('company-id'));
+                }
+            })
+
             ->where(function ($query) use ($request, $companyIds) {
                 if (auth()->user()->getGroupNameByEmpresaId($request->header('company-id')) == 'Consultor') {
                     $query->whereIn('company_id',  $companyIds);
                 }
             })
 
-            ->where(function ($query) use ($request) {
-                if (auth()->user()->getGroupNameByEmpresaId(empresaId: $request->header('company-id')) != 'Consultor') {
-                    $query->where('company_id',  $request->header('company-id'));
-                }
-            })
+            
 
             ->join('emprestimos', 'parcelas.emprestimo_id', '=', 'emprestimos.id')
             ->join('clients', 'emprestimos.client_id', '=', 'clients.id')
