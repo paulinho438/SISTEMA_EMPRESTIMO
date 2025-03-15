4e7293080a50e497f1bdafcdb3dffcdaeb262f16
Paulo Peixoto <paulo.peixoto@gs3tecnologia.com.br>
2025-03-15 10:55:25 -0300

    a

--- api/database/migrations/2025_03_15_102425_create_parcelas_table.php
+++ api/database/migrations/2025_03_15_102425_create_parcelas_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-
+        Schema::table('parcelas', function (Blueprint $table) {
+            $table->timestamp('ult_dt_geracao_pix')->nullable();
+        });
 
         Schema::table('quitacao', function (Blueprint $table) {
             $table->timestamp('ult_dt_geracao_pix')->nullable();
