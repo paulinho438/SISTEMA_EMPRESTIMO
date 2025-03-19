1cf38f2bac9c63472b17a0b96cc4777a78e2fc63
Paulo Peixoto <paulo.peixoto@gs3tecnologia.com.br>
2025-03-18 21:43:36 -0300

    a

--- api/app/Http/Resources/PagamentoMinimoResource.php
+++ api/app/Http/Resources/PagamentoMinimoResource.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Resources\Json\JsonResource;
 use App\Models\Permgroup;
 
 use DateTime;
-use Carbon\Carbon;
 
 class PagamentoMinimoResource extends JsonResource
 {
@@ -19,18 +18,6 @@ class PagamentoMinimoResource extends JsonResource
      */
     public function toArray($request)
     {
-        $dtLancamentoHoje = $this->dt_ult_cobranca ? (new DateTime($this->dt_ult_cobranca))->format('Y-m-d') === Carbon::now()->format('Y-m-d') : false;
-
-        // Definindo chave_pix com base na lógica fornecida
-        $chave_pix = null;
-        if ($dtLancamentoHoje) {
-            $chave_pix = $this->chave_pix;
-        } elseif ($this->emprestimo->banco->wallet == 1) {
-            $chave_pix = '';
-        } else {
-            $chave_pix = $this->emprestimo->banco->chavepix;
-        }
-
         return [
             "id"                    => $this->id,
             "emprestimo_id"         => $this->emprestimo_id,
@@ -38,8 +25,7 @@ class PagamentoMinimoResource extends JsonResource
             "valorSemFormatacao"    => $this->valor,
             "dt_baixa"              => ($this->dt_baixa != null) ? (new DateTime($this->dt_baixa))->format('d/m/Y') : '',
             "identificador"         => $this->identificador,
-            "chave_pix"             => $chave_pix,
-            "ult_dt_geracao_pix" => $this->ult_dt_geracao_pix ? (new DateTime($this->ult_dt_geracao_pix))->format('d/m/Y') : null,
+            "chave_pix"             => ($this->chave_pix != null) ? $this->chave_pix : '',
         ];
     }
 
