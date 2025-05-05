87eb49d2929cc17b744ec6b385bf590e978cf576
Paulo Peixoto <paulo.peixoto@gs3tecnologia.com.br>
2025-05-05 20:35:48 -0300

    h

--- api/app/Console/Kernel.php
+++ api/app/Console/Kernel.php
@@ -49,7 +49,7 @@ class Kernel extends ConsoleKernel
 
         //$schedule->command('recalcular:Parcelas')->dailyAt('00:00');
 
-        //$schedule->command('recalcular:Parcelas')->everyTenMinutes()->withoutOverlapping();
+        $schedule->command('recalcular:Parcelas')->everyTenMinutes()->withoutOverlapping();
 
         $schedule->command('cobranca:AutomaticaA')->dailyAt('08:00');
         $schedule->command('cobranca:AutomaticaB')->dailyAt('13:00');
