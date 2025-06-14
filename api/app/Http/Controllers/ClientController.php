1ae3703a73dcec6204c0a51839b14aeb51f1440a
Paulo Peixoto <paulo.peixoto@gs3tecnologia.com.br>
2025-06-14 14:10:57 -0300

    a

--- api/app/Http/Controllers/ClientController.php
+++ api/app/Http/Controllers/ClientController.php
@@ -431,10 +431,10 @@ class ClientController extends Controller
         $emprestimos = $user->emprestimos()
             ->where('company_id', $request->header('company-id'))
             ->whereHas('parcelas', function ($query) {
-                $query->whereNull('dt_baixa'); // Filtra empréstimos com parcelas pendentes
+                $query->whereNotNull('dt_baixa'); // Filtra empréstimos com parcelas pendentes
             })
             ->with(['parcelas' => function ($query) {
-                $query->whereNull('dt_baixa'); // Carrega apenas parcelas pendentes
+                $query->whereNotNull('dt_baixa'); // Carrega apenas parcelas pendentes
             }])
             ->get();
 
